ecc784ee1178d8c4904f8262fd65261f6a6847e2
NicoVigna <153986252+NicoVigna@users.noreply.github.com>
2024-02-17 11:38:13 +0000

    Salvataggio 5 ora

--- Tris/Tris.cpp
+++ Tris/Tris.cpp
@@ -63,11 +63,69 @@ public:
             cout << endl;
         }
     }
+    int controlla_vincitore()
+    {
+        int risultato;
+        for (int i = 0; i < 3; i++)
+        {
+            risultato = controlla_colonna(i);
+            if (risultato != 0)
+                return risultato;
+            risultato = controlla_riga(i);
+            if (risultato != 0)
+                return risultato;
+        }
+    }
+
+private:
+    int controlla_colonna(int col)
+    {
+        int acc_uno = 0;
+        int acc_due = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            int cella = griglia[i][col];
+            if (cella == 1)
+                acc_uno++;
+            else if (cella == 2)
+                acc_due++;
+        }
+
+        if (acc_uno == 3)
+            return 1;
+        if (acc_due == 3)
+            return 2;
+    }
+
+    int controlla_riga(int rig)
+    {
+        int acc_uno = 0;
+        int acc_due = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            int cella = griglia[rig][i];
+            if (cella == 1)
+                acc_uno++;
+            else if (cella == 2)
+                acc_due++;
+        }
+
+        if (acc_uno == 3)
+            return 1;
+        if (acc_due == 3)
+            return 2;
+    }
+
+    int controlla_diagonali()
+    {
+    }
 };
 
 int main(int argc, char const *argv[])
 {
-    int x, y;
+    int x, y, vincitore;
+    int mosse_totali = 0;
     bool mossa_valida;
     Tris myTris;
     myTris.reset_tabella();
@@ -75,28 +133,43 @@ int main(int argc, char const *argv[])
     cout << "stato tabella iniziale!" << endl;
     myTris.stampa_griglia();
 
-    do
+    while (mosse_totali < 9)
     {
-        cout << "mossa del giocatore 1" << endl;
-        cout << "x: ";
-        cin >> x;
-        cout << "y: ";
-        cin >> y;
-        mossa_valida = myTris.giocatore_uno(x, y);
-    } while (!mossa_valida);
+        do
+        {
+            cout << "mossa del giocatore 1" << endl;
+            cout << "x: ";
+            cin >> x;
+            cout << "y: ";
+            cin >> y;
+            mossa_valida = myTris.giocatore_uno(x, y);
+        } while (!mossa_valida);
 
-    myTris.stampa_griglia();
-
-    do
-    {
-        cout << "mossa del giocatore 2" << endl;
-        cout << "x: ";
-        cin >> x;
-        cout << "y: ";
-        cin >> y;
-        mossa_valida = myTris.giocatore_due(x, y);
-    } while (!mossa_valida);
-    myTris.stampa_griglia();
+        myTris.stampa_griglia();
+        vincitore = myTris.controlla_vincitore();
+        if (vincitore != 0)
+            break;
+        do
+        {
+            cout << "mossa del giocatore 2" << endl;
+            cout << "x: ";
+            cin >> x;
+            cout << "y: ";
+            cin >> y;
+            mossa_valida = myTris.giocatore_due(x, y);
+        } while (!mossa_valida);
+        myTris.stampa_griglia();
+        vincitore = myTris.controlla_vincitore();
+        if (vincitore != 0)
+            break;
+        mosse_totali += 2;
+    }
+    if (vincitore == 1)
+        cout << "Vince il giocatore 1";
+    else if (vincitore == 2)
+        cout << "Vince il giocatore 2";
+    else
+        cout << "Non ha vinto nessuno";
 
     return 0;
 }
