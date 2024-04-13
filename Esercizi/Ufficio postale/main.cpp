c0b884966ffa1849e37e8f2a3e6e8009b89b64ef
Nicolò Vignali <153986252+NicoVigna@users.noreply.github.com>
2024-04-13 10:08:17 +0000

    salvataggio

---
+++ Esercizi/Ufficio postale/main.cpp
@@ -0,0 +1,23 @@
+/*
+*Vignali Nicolò-3CIN
+*06/04/2024
+*Esercizio 16 A
+*/
+
+#include <iostream>
+using namespace std;
+#include "UfficioPostale.cpp"
+
+int main(){
+UfficioPostale up = UfficioPostale("Saliceto");
+up.nuovoCliente('f');
+up.nuovoCliente('f');
+up.nuovoCliente('r');
+up.nuovoCliente('s');
+up.nuovoCliente('r');
+up.nuovoCliente('s');
+up.nuovoCliente('r');
+up.nuovoCliente('s');
+up.nuovoCliente('F');
+up.stampaTabellone();
+}
